81a93c5ff8f3e789eff4099f2187424362ee72ed
yashsharma6391 <ys7097569@gmail.com>
2025-11-17 16:36:21 +0530

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -79,15 +79,22 @@ router.post("/login", async (req, res) => {
 // ------------------------------
 // MIDDLEWARE TO PROTECT ROUTES
 // ------------------------------
-export function adminAuth(req, res, next) {
-  const token = req.cookies.adminToken;
+export async function adminAuth(req, res, next) {
+  const token = req.cookies?.adminToken;
 
   if (!token) return res.status(401).json({ message: "Not authorized" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const admin = await Admin.findById(decoded.id);
+    if (!admin) {
+      // return res.status(404).json({ message: "Admin not found" });
+       res.clearCookie("adminToken", { httpOnly: true, secure: true, sameSite: "none" });
+      return res.status(404).json({ message: "Admin not found" });
+    }
+    
     req.adminId = decoded.id;
-
+     req.admin = admin;
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
