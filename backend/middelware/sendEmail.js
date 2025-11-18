f19fbe8b375b995cedc519492ee55dac03124a8a
yashsharma6391 <ys7097569@gmail.com>
2025-11-18 22:18:55 +0530

    Update sendEmail.js

--- backend/middelware/sendEmail.js
+++ backend/middelware/sendEmail.js
@@ -2,10 +2,10 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
-     host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-    // service: process.env.SERVICE, // or your SMTP service
+  //    host: "smtp.gmail.com",
+  // port: 587,
+  // secure: false,
+    service: process.env.SERVICE, // or your SMTP service
     auth: {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD,
